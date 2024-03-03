d701f9bb68f8d76200544c310d2f061b5e14de46
sem00009@mix.wvu.edu <kianmcchesney@gmail.com>
2024-03-03 11:44:51 -0500

    test 3, code directly from bootstrap

--- src/Card.js
+++ src/Card.js
@@ -3,32 +3,31 @@ import React from 'react';
 const Card = () => {
   return (
     <div class="card-group">
-        <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-         <div class="card-header">Card 01</div>
-         <div class="card-body">
-         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <div class="card">
+    <img class="card-img-top" src="..." alt="Card image cap"/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
-    
-
-    <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
-  <div class="card-header">Card 02</div>
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="..." alt="Card image cap"/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="..." alt="Card image cap"/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
   </div>
 </div>
-
-<div class="card text-white bg-success mb-3" style="max-width: 18rem;">
-  <div class="card-header">Card 03</div>
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-
-</div>
-
-
-
-    </div>
   );
 }
 
