3ac73966d65d7092ffde8f8dc5309781e27305bc
sem00009@mix.wvu.edu <kianmcchesney@gmail.com>
2024-03-03 11:39:05 -0500

    deploy test 2

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import Card from './Card';
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
+    <Navbar/>
       <h1>CS 230L</h1>
       <h2>Section -01</h2>
       <p>WVU ID: 800334528</p>
       <p>Hi, I am Kian</p>
-      <Card></Card>
+      <Card/>
     </div>
   );
 }
