603884e6ea50a33a06bc30427457ba453285de70
sem00009@mix.wvu.edu <kianmcchesney@gmail.com>
2024-03-03 19:38:58 -0500

    fixed error

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,14 +8,14 @@ const Navbar = () => {
     <div class="navbar-header">
       <a class="navbar-brand" href="#">WebSiteName</a>
     </div>
-    <ul class="nav navbar-nav">
+    <ul class="nav navbar-nav"> 
       <li class="active"><a href="#">Home</a></li>
       <li><a href="#">Page 1</a></li>
       <li><a href="#">Page 2</a></li>
     </ul>
     <form class="navbar-form navbar-left" action="/action_page.php">
       <div class="form-group">
-        <input type="text" class="form-control" placeholder="Search">
+        <input type="text" class="form-control" placeholder="Search"/>
       </div>
       <button type="submit" class="btn btn-default">Submit</button>
     </form>
