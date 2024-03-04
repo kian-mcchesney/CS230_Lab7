6613562eb68e4891890428aed1eb3e2efe401e35
sem00009@mix.wvu.edu <kianmcchesney@gmail.com>
2024-03-03 19:51:53 -0500

    Added text to cards

--- src/Card.js
+++ src/Card.js
@@ -4,18 +4,25 @@ const Card = () => {
   return (
 <div className="card-group">
 <div class="card bg-primary text-white">
-  <div class="card-header">Header</div>
-  <div class="card-body">Content</div>
+  <div class="card-header">Card 01</div>
+  <div class="card-body">
+     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  
  
 </div>
 <div class="card bg-danger text-white">
-  <div class="card-header">Header</div>
-  <div class="card-body">Content</div>
+  <div class="card-header">Card 02</div>
+  <div class="card-body">
+  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
   
 </div>
 <div class="card bg-success text-white">
-  <div class="card-header">Header</div>
-  <div class="card-body">Content</div>
+  <div class="card-header">Card 03</div>
+  <div class="card-body">
+  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
  
 </div>
    
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,26 +3,29 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
-    <ul class="nav navbar-nav"> 
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
     </ul>
-    <form class="navbar-form navbar-left" action="/action_page.php">
-      <div class="form-group">
-        <input type="text" class="form-control" placeholder="Search"/>
-      </div>
-      <button type="submit" class="btn btn-default">Submit</button>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
 </nav>
-
-
 
 );
 }
