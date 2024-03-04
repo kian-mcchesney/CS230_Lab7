02366352d1340b1fd7bfcb78708873d658a51cce
sem00009@mix.wvu.edu <kianmcchesney@gmail.com>
2024-03-03 20:29:44 -0500

    Fixed nav bar, finally learned npm start

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,13 +3,14 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-<nav class="navbar navbar-expand-lg navbar-dark bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
+    <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="#">Home </a>
       </li>
@@ -20,12 +21,11 @@ const Navbar = () => {
         <a class="nav-link" href="#">Contact</a>
       </li>
     </ul>
-    <div class="ml-auto">
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-    </div>
+    <form class="d-flex float-right">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+  </div>
   </div>
 </nav>
 
