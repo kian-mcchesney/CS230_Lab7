5f07bdb7f0eb77978181411a1185a9e799a294c2
sem00009@mix.wvu.edu <kianmcchesney@gmail.com>
2024-03-03 20:01:52 -0500

    Changed navbar color and aligned search

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,21 +3,22 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-light">
   <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
+        <a class="nav-link disabled" href="#">About</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Contact</a>
+        <a class="nav-link disabled" href="#">Contact</a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
@@ -26,7 +27,6 @@ const Navbar = () => {
     </form>
   </div>
 </nav>
-
 );
 }
 
